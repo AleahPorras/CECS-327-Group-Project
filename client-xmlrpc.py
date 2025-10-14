48d047462d64929a0e4636691799bcad16bf35a5
Kristen Masters <kristen.masters01@student.csulb.edu>
2025-10-13 20:56:07 -0700

    Fixed errors with syntax, will be fixing up the functions a bit. Right now we have the dictionary that stores all the rooms and thir members, along with storing owner. We can keep track of when someone joins a member list and when someone creates a room.

--- client-xmlrpc.py
+++ client-xmlrpc.py
@@ -6,11 +6,21 @@ proxy = xmlrpc.client.ServerProxy('http://localhost:3000')
 
 
 def get_room_to_join():
-    room_to_join = input("Enter the name of the room you would likee to join: ")
+    room_to_join = input("Enter the name of the room you would like to join: ")
+    # Checks if the user provided a name for the room
+    while room_to_join == None: 
+        print("No name provided, try again.")
+        room_to_join = input("Enter the name of the room you would like to join: ")
+        
     return room_to_join
 
-def get_room_to_create(room_to_create):
+def get_room_to_create():
     room_to_create = input("Enter the name of the room you want to create:") 
+    # Checks if the user provided a name for the room
+    while room_to_create == None: 
+        print("No name provided, try again.")
+        room_to_create = input("Enter the name of the room you would like to join: ")
+        
     return room_to_create
 
 if __name__ == '__main__':
@@ -18,18 +28,25 @@ if __name__ == '__main__':
     #Calling the remote function and printing the list (place holder function)
     ## We will call our functions like this 
     # print(proxy.list_directory())
-    user = input("What is your room name: ")
+
+    user = input("What is your username?: ")
     action = input("Would you like to join or create a room? (join/create): ")
     
-    if action == "join":
-        
+    if action == "join" or action == "Join":
         room = get_room_to_join()
-
-        print(proxy.join_room(room))
+        print("Joining the room...")
+        output = proxy.join_room(room, user)
+        print(f"{user} you have joined {room}")
+        print(output)
         
-    if action == "create":
+    elif action == "create" or action == "Create":
 
         room = get_room_to_create()
-        print(proxy.create_room(room,user))
-        
+        print("Creating the room...")
+        output = proxy.create_room(room, user)
+        print(output)
+
+    else: 
+        print("You have not entered a valued response, please run again. ")
+        print("Hint: You must have it printed exactly, either join or create")
 
--- server-xmlrpc.py
+++ server-xmlrpc.py
@@ -1,9 +1,9 @@
 import xmlrpc.server 
 import os
 
-#___________________________     INITIALIZING     __________________________
+#!___________________________     INITIALIZING     __________________________
 #For the list_directory() function
-directory = os.path.dirname(os.path.abspath(__file__))
+#directory = os.path.dirname(os.path.abspath(__file__))
 
 #creation of the server (listens to the address)
 server = xmlrpc.server.SimpleXMLRPCServer(('localhost',3000), logRequests = True)
@@ -11,38 +11,53 @@ server = xmlrpc.server.SimpleXMLRPCServer(('localhost',3000), logRequests = True
 
 list_of_chat_rooms = {}
 
-#___________________________ FUNCTION DEFINITIONS __________________________
+#!___________________________ FUNCTION DEFINITIONS __________________________
 ## Example function, we need to create our own functions 
 # def list_directory():
 #     return os.listdir(directory)
 
-def create_room(room_to_create): # later on will implement an "owned by:" parameter
+
+#& FUNCTION THAT CREATES A NEW ROOM, ADDS THE CREATOR TO ROOM
+def create_room(room_to_create, user): # later on will implement an "owned by:" parameter
     """Creates a new chatroom that different clients can join. Will check if a chatroom with the same name already exists."""
     # Appends the new chatroom to the list of existing chatrooms
     list_of_chat_rooms[room_to_create] = {
-        "members": set(), # where we will store the members of the chatroom
+        'owner': user,
+        'members': [], # where we will store the members of the chatroom
     }
-    print(f"Chatroom {room_to_create} has been created.")
+    # Confirmation message that chatroom has been created
+    print(f"Chatroom {room_to_create} has been created.\n")
 
+    list_of_chat_rooms[room_to_create]['members'].append(user)
+    # Confirmation message that chatroom owner has entered the room.
+    print("BELLO BELLO BELLO BELLO WELCOME TO THE START OF YOUR CHAT HISTORY!!")
+    print(f"Chatroom owner {user} has entered the room {room_to_create}.")
+    return list_of_chat_rooms
+
+
+#& FUNCTION THAT ADDS A USER TO A ROOM
 def join_room(room_to_join, user): 
-    """Will allow the user to join rooms they are intrested in 
+    """Will allow the user to join rooms they are intrested in."""
+
+    list_of_chat_rooms[room_to_join]['members'].append(user)
+    # Confirmation message that a user has entered the chatroom.
     
-    """
-    pass
+    updated_members = list_of_chat_rooms[room_to_join]['members']
+    print(f"Welcome {user}! You have entered the room {room_to_join}.")
+
+    return updated_members
 
 
 
-#___________________________ FUNCTION REGISTRATION ___________________________
+#!___________________________ FUNCTION REGISTRATION ___________________________
 #registering the function 
 # server.register_function(list_directory)
-# server.register_function(create_room)
-# server.register_function(join_room)
+server.register_function(create_room)
+server.register_function(join_room)
 
 
 
-
-
-#___________________________     SERVER CALLING    ___________________________
+#!___________________________     SERVER CALLING    ___________________________
 if __name__ == '__main__': 
     try:
         ## Successful 

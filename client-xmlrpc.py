cdb152eea99c8c02cef680e00c5ecc7259e87655
Aleah P. <92504607+AleahPorras@users.noreply.github.com>
2025-10-13 18:53:35 -0700

    Working on RPC, created two functions: create_room and join_room (in server-xmlrpc.py) that will act as the RPC functions which will be called in the client-xmlrpc.py file.  - Aleah & Kristen

--- client-xmlrpc.py
+++ client-xmlrpc.py
@@ -1,12 +1,35 @@
-from  xmlrpc.client import ServerProxy
+import  xmlrpc.client 
 
 #acessing the server
-proxy = ServerProxy('http://localhost:3000')
+proxy = xmlrpc.client.ServerProxy('http://localhost:3000')
+
+
+
+def get_room_to_join():
+    room_to_join = input("Enter the name of the room you would likee to join: ")
+    return room_to_join
+
+def get_room_to_create(room_to_create):
+    room_to_create = input("Enter the name of the room you want to create:") 
+    return room_to_create
 
 if __name__ == '__main__':
+    ##EXAMPLE CALLING
     #Calling the remote function and printing the list (place holder function)
     ## We will call our functions like this 
-    print(proxy.list_directory())
+    # print(proxy.list_directory())
+    user = input("What is your room name: ")
+    action = input("Would you like to join or create a room? (join/create): ")
+    
+    if action == "join":
+        
+        room = get_room_to_join()
 
+        print(proxy.join_room(room))
+        
+    if action == "create":
 
+        room = get_room_to_create()
+        print(proxy.create_room(room,user))
+        
 
--- server-xmlrpc.py
+++ server-xmlrpc.py
@@ -1,4 +1,4 @@
-from xmlrpc.server import SimpleXMLRPCServer
+import xmlrpc.server 
 import os
 
 #___________________________     INITIALIZING     __________________________
@@ -6,20 +6,38 @@ import os
 directory = os.path.dirname(os.path.abspath(__file__))
 
 #creation of the server (listens to the address)
-server = SimpleXMLRPCServer(('localhost',3000), logRequests = True)
+server = xmlrpc.server.SimpleXMLRPCServer(('localhost',3000), logRequests = True)
 
 
+list_of_chat_rooms = {}
 
 #___________________________ FUNCTION DEFINITIONS __________________________
 ## Example function, we need to create our own functions 
-def list_directory():
-    return os.listdir(directory)
+# def list_directory():
+#     return os.listdir(directory)
+
+def create_room(room_to_create): # later on will implement an "owned by:" parameter
+    """Creates a new chatroom that different clients can join. Will check if a chatroom with the same name already exists."""
+    # Appends the new chatroom to the list of existing chatrooms
+    list_of_chat_rooms[room_to_create] = {
+        "members": set(), # where we will store the members of the chatroom
+    }
+    print(f"Chatroom {room_to_create} has been created.")
+
+def join_room(room_to_join, user): 
+    """Will allow the user to join rooms they are intrested in 
+    
+    """
+    pass
 
 
 
 #___________________________ FUNCTION REGISTRATION ___________________________
 #registering the function 
-server.register_function(list_directory)
+# server.register_function(list_directory)
+# server.register_function(create_room)
+# server.register_function(join_room)
+
 
 
 
@@ -28,10 +46,11 @@ server.register_function(list_directory)
 if __name__ == '__main__': 
     try:
         ## Successful 
-        print('Serving divorce papers...')
+        print('Connecting to ThisCord Server...')
         #starting the server
         server.serve_forever()
 
     except KeyboardInterrupt:
         ## Not Sucessful
-        print('Getting your children taken away')
+        print('Unable to connect, try again later')
+

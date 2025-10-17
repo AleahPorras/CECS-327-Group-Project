41700fc3dc6ebf6c6134a53c2c3bd1359f9f9af5
Aleah P. <92504607+AleahPorras@users.noreply.github.com>
2025-10-17 00:19:12 -0700

    Started the process of combining RPC, PubSub, and IPC; Not finished, will clean up code later; will delete server.py & client.py later or move them to a seperate folder in the repo just for safekeeping.

--- client-xmlrpc.py
+++ client-xmlrpc.py
@@ -1,8 +1,32 @@
 import  xmlrpc.client 
+import zmq
+import json
+import threading
 
-#acessing the server
-proxy = xmlrpc.client.ServerProxy('http://localhost:3000')
+def message_subscriber(chatroom_name, user):
+    BROKER_PUB = "tcp://127.0.0.1:5556" # connects to the broker
+    TOPIC = f"room/{chatroom_name}" # subscribes to a topic/prefix
 
+    ctx = zmq.Context.instance() # get zmq context
+    sub = ctx.socket(zmq.SUB)   # create a SUB socket
+    sub.connect(BROKER_PUB)
+    sub.setsockopt_string(zmq.SUBSCRIBE, TOPIC) # install a topic/prefix filter
+    print(f"[Client] Subscribed to messages in {chatroom_name}.")
+    print("> ", end ="", flush=True)
+
+    while True:
+        try:
+            # Receives messages from the broker
+            broker_topic, broker_payload = sub.recv_multipart()
+            topic = broker_topic.decode("utf-8")
+            payload = json.loads(broker_payload.decode("utf-8"))
+
+            # Ensures that the user's messages don't echo back to them
+            if payload.get("user") != user:
+                print(f"{payload['user']}: {payload['text']}\n> ", end ="",flush=True)
+
+        except (KeyboardInterrupt, zmq.ZMQError):
+            break
 
 #----------------------functions ----------------------
 def get_room_to_join():
@@ -23,95 +47,137 @@ def get_room_to_create():
         
     return room_to_create
 
-if __name__ == '__main__':
-    ##EXAMPLE CALLING
-    #Calling the remote function and printing the list (place holder function)
-    ## We will call our functions like this 
-    # print(proxy.list_directory())
+def main():
 
-    user = input("What is your username?: ")
-    action = input("Would you like to join or create a room? (join/create): ")
-    #---------------------------joining a room------------------------------
-    if action == "join" or action == "Join":
-        room = get_room_to_join()
+    #acessing the server
+    proxy = xmlrpc.client.ServerProxy('http://localhost:3000')
 
-        #Calls the function from the server
-        current_rooms = proxy.all_rooms()
-        
-        #Error handling for when a user tries to access a room that doesnt exist 
-        # * Will ask the user if they want to create the room with that name 
-        #   - Then creates the room by calling the function from the client 
-        # * Will ask if the user would rather put in a new name in case they put in the wrong name 
-        #   by mistake 
-        while room not in current_rooms: 
-            answer = input("This room does not exist, would you like to create it? yes/no: ")
-            if answer == "Yes" or answer == 'yes': 
-               
-                print("Creating the room...\n")
-                output = proxy.create_room(room, user)
-                print(f'List of all current rooms and members: {output}\n')
-                print("Welcome to your new room!")
-                
-                
-            
-               
-            elif answer == "No" or answer == 'no':
-                room = get_room_to_join()
-                current_members = proxy.current_members(room)
-               
+    try:
+        user = input("What is your username?: ")
 
-            # repeats if the user puts in a response that isn't correct 
-            else: 
-                print("Not a correct response try again... ")
-
+        chatroom = None
+        while not chatroom:
+            action = input("Would you like to join or create a room? (join/create): ")
             current_rooms = proxy.all_rooms()
+            if action == "create" or action == "Create":
+                chatroom_name = get_room_to_create()
+                if proxy.create_room(chatroom_name, user):
+                    print(f"Creating chatroom {chatroom_name}...\n")
+                    chatroom = chatroom_name
+                else:
+                    print(f"Chatroom {chatroom_name} exists, join it instead.\n")
+            
+            elif action == "join" or action == "Join":
+                print(f"Available Chatrooms:\n{current_rooms}")
+                chatroom_name = get_room_to_join()
+                if chatroom_name in current_rooms:
+                    chatroom = chatroom_name
+                else:
+                    print(f"Chatroom {chatroom_name} does not exist.\n")
+            else:
+                print("You have not entered a valued response, please run again. ")
+                print("Hint: You must have it printed exactly, either join or create")
+
+        proxy.join_room(chatroom, user)
+
+        thread = threading.Thread(target = message_subscriber, args = (chatroom, user), daemon=True)
+        thread.start()
+
+        while True:
+            message = input()
+            if message == 'exit' or message == 'Exit':
+                print("\nExiting chat...")
+                break
+            proxy.send_message(chatroom, user, message)
+            print("> ", end = "",flush=True)
+
+    except KeyboardInterrupt:
+        print("\nExiting chat...")
+
+if __name__ == '__main__': 
+    main()
+#     ##EXAMPLE CALLING
+#     #Calling the remote function and printing the list (place holder function)
+#     ## We will call our functions like this 
+#     # print(proxy.list_directory())
+
+#     user = input("What is your username?: ")
+#     action = input("Would you like to join or create a room? (join/create): ")
+#     #---------------------------joining a room------------------------------
+#     if action == "join" or action == "Join":
+#         room = get_room_to_join()
+
+#         #Calls the function from the server
+#         current_rooms = proxy.all_rooms()
         
+#         #Error handling for when a user tries to access a room that doesnt exist 
+#         # * Will ask the user if they want to create the room with that name 
+#         #   - Then creates the room by calling the function from the client 
+#         # * Will ask if the user would rather put in a new name in case they put in the wrong name 
+#         #   by mistake 
+#         while room not in current_rooms: 
+#             answer = input("This room does not exist, would you like to create it? yes/no: ")
+#             if answer == "Yes" or answer == 'yes': 
+                
+#                 print("Creating the room...\n")
+#                 output = proxy.create_room(room, user)
+#                 print(f'List of all current rooms and members: {output}\n')
+#                 print("Welcome to your new room!")
+                
+#             elif answer == "No" or answer == 'no':
+#                 room = get_room_to_join()
+#                 current_members = proxy.current_members(room)
+            
+#             # repeats if the user puts in a response that isn't correct 
+#             else: 
+#                 print("Not a correct response try again... ")
+#             current_rooms = proxy.all_rooms()
+            
+#         # Carries out for all attmepts 
+#         print("\n Joining the room...\n")
 
-        # Carries out for all attmepts 
-        print("\n Joining the room...\n")
+#         # fetching the current list of members 
+#         current_members = proxy.current_members(room)
 
-        # fetching the current list of members 
-        current_members = proxy.current_members(room)
+#         # adding the user to the room
+#         output = proxy.join_room(room, user)
+#         print(f"List of all current members: {current_members}\n")
 
-        # adding the user to the room
-        output = proxy.join_room(room, user)
-        print(f"List of all current members: {current_members}\n")
+#         # verification for the user that they have been added
+#         print(f"{user} you have joined {room}\n")
 
-        # verification for the user that they have been added
-        print(f"{user} you have joined {room}\n")
-      
-        # getting the current list of users in the room
-        current_members = proxy.current_members(room)
+#         # getting the current list of users in the room
+#         current_members = proxy.current_members(room)
 
-        #returns to terminal to make sure that the list has changed from before
-        print(f"List of all current members: {current_members}\n")
-               
+#         #returns to terminal to make sure that the list has changed from before
+#         print(f"List of all current members: {current_members}\n")
         
+    
+#     #-----------------------------creating a room-------------------------------- 
+#     elif action == "create" or action == "Create":
 
-       
-    #-----------------------------creating a room-------------------------------- 
-    elif action == "create" or action == "Create":
+#         # calling client function that asks the user for a room they would want to create
+#         room = get_room_to_create()
 
-        # calling client function that asks the user for a room they would want to create
-        room = get_room_to_create()
+#         # gets the state of all rooms before new room is added
+#         rooms_before = proxy.all_rooms()
+#         print(f'Current list before adding: {rooms_before}\n')
 
-        # gets the state of all rooms before new room is added
-        rooms_before = proxy.all_rooms()
-        print(f'Current list before adding: {rooms_before}\n')
-
-        print("Creating the room...\n")
+#         print("Creating the room...\n")
         
-        # creates a new room and makes them thw owner 
-        output = proxy.create_room(room, user)
-        # adds the owner to the list of members
-        proxy.join_room(room, user)
-        # gets the updated list of members in the room
-        current_rooms =  proxy.all_rooms()
+#         # creates a new room and makes them thw owner 
+#         output = proxy.create_room(room, user)
+#         # adds the owner to the list of members
+#         proxy.join_room(room, user)
+#         # gets the updated list of members in the room
+#         current_rooms =  proxy.all_rooms()
 
-        #prints out the list of members
-        print(f'List of all current rooms and members: {current_rooms}\n')
+#         #prints out the list of members
+#         print(f'List of all current rooms and members: {current_rooms}\n')
 
-    else: 
-        print("You have not entered a valued response, please run again. ")
-        print("Hint: You must have it printed exactly, either join or create")
+#     else: 
+#         print("You have not entered a valued response, please run again. ")
+#         print("Hint: You must have it printed exactly, either join or create")
+
+    
 
--- server-xmlrpc.py
+++ server-xmlrpc.py
@@ -1,5 +1,6 @@
 import xmlrpc.server 
-import os
+import zmq
+import json
 
 #!___________________________     INITIALIZING     __________________________
 #For the list_directory() function
@@ -8,7 +9,14 @@ import os
 #creation of the server (listens to the address)
 server = xmlrpc.server.SimpleXMLRPCServer(('localhost',3000), logRequests = True)
 
+# merging from publisher.py
+BROKER = "tcp://127.0.0.1:5555"
+ctx = zmq.Context.instance()
+push = ctx.socket(zmq.PUSH) # pushes messages to the broker.py
+push.connect(BROKER)
+print(f"[Server] Connected to broker at {BROKER}.")
 
+# Keeps track of all chat rooms, members, and owners
 list_of_chat_rooms = {}
 
 #!___________________________ FUNCTION DEFINITIONS __________________________
@@ -19,7 +27,7 @@ list_of_chat_rooms = {}
 # returns  the state of all rooms, the rooms, members, and owners
 def all_rooms():
     
-    return list_of_chat_rooms
+    return list(list_of_chat_rooms.keys())
 
 #returns the state of just the members of a room
 def current_members(room_to_join):
@@ -29,6 +37,11 @@ def current_members(room_to_join):
 #& FUNCTION THAT CREATES A NEW ROOM, ADDS THE CREATOR TO ROOM
 def create_room(room_to_create, user): # later on will implement an "owned by:" parameter
     """Creates a new chatroom that different clients can join. Will check if a chatroom with the same name already exists."""
+
+    # Error message when a chatroom that already exists is inputed
+    if room_to_create in list_of_chat_rooms:
+        return f"Error: The chatroom {room_to_create} already exists."
+
     # Appends the new chatroom to the list of existing chatrooms
     list_of_chat_rooms[room_to_create] = {
         'owner': [],
@@ -47,6 +60,10 @@ def create_room(room_to_create, user): # later on will implement an "owned by:" 
 def join_room(room_to_join, user): 
     """Will allow the user to join rooms they are intrested in."""
 
+    # Error message when a chatroom that does not exist is inputed
+    if room_to_join not in list_of_chat_rooms:
+        return f"Error: The chatroom {room_to_join} does not exist."
+
     # appends the current user to the members
     list_of_chat_rooms[room_to_join]['members'].append(user)
 
@@ -58,19 +75,34 @@ def join_room(room_to_join, user):
     # returns the new list of members
     return updated_members
 
+#& FUNCTION THAT SENDS MESSAGE TO THE BROKER
+def send_message(chatroom_name, user, message):
+    # Error message when a chatroom that does not exist is inputed
+    if chatroom_name not in list_of_chat_rooms:
+        return f"Error: The chatroom {chatroom_name} does not exist."
+    
+    topic = f"room/{chatroom_name}"
+
+    payload = {"user": user, "text": message}
+
+    # Formats the message 
+    message_to_broker = {"topic": topic, "payload": payload}
+
+    # Pushes message to the broker
+    push.send_json(message_to_broker)
+    print(f"Sent message to chatroom {topic}: {payload}")
+    return True
 
 
 #!___________________________ FUNCTION REGISTRATION ___________________________
-#registering the function 
-
+#registering the functions
 
 server.register_function(all_rooms)
+server.register_function(send_message)
 server.register_function(current_members)
 
 server.register_function(create_room)
 server.register_function(join_room)
-
-
 
 
 #!___________________________     SERVER CALLING    ___________________________
